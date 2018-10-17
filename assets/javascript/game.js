ce2d82a4e4d27c8cdd99305e2e8b210907ca7bc2
jebizar <jebizar@gmail.com>
2018-10-16 17:32:43 -0700

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+
+
+var listOfWords = ["bootcamp", "coding", "language", "developer"];
+
+var word = listOfWords[Math.floor(Math.random() * listOfWords.length)];
+
+var blank = document.getElementById("hidden");
+
+var tries = document.getElementById("tries");
+
+var remaining = 10;
+
+var hiddenLetter = [];
+for (i = 0; i < word.length; i++) {
+    hiddenLetter[i] = "_";
+    blank.textContent = hiddenLetter;
+}
+
+console.log(listOfWords);
+document.onkeyup = function (event) {
+    var letter = event.key;
+    for (j = 0; j < word.length; j++) {
+        if (word[j] == letter) {
+            hiddenLetter[j] = letter;
+            blank.textContent = hiddenLetter;
+        }
+    }
+    if (word.indexOf(letter) < 0) {
+        remaining--;
+    }
+    tries.textContent = remaining;
+
+    
+}
+
+
+
+
