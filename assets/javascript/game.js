85c7bcd6db15b458b714b09fd74cac96c89112dc
jebizar <jebizar@gmail.com>
2018-10-19 13:12:16 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,70 @@
-
-
-var listOfWords = ["bootcamp", "coding", "language", "developer"];
-
-var word = listOfWords[Math.floor(Math.random() * listOfWords.length)];
-
 var blank = document.getElementById("hidden");
 
 var tries = document.getElementById("tries");
 
-var remaining = 10;
+var guess = document.getElementById("guessed");
 
-var hiddenLetter = [];
-for (i = 0; i < word.length; i++) {
-    hiddenLetter[i] = "_";
-    blank.textContent = hiddenLetter;
+var wins = document.getElementById("winCount");
+
+var losses = document.getElementById("lossCount");
+
+count = {
+    winC: 0,
+    lossC: 0
 }
 
-console.log(listOfWords);
-document.onkeyup = function (event) {
-    var letter = event.key;
-    for (j = 0; j < word.length; j++) {
-        if (word[j] == letter) {
-            hiddenLetter[j] = letter;
-            blank.textContent = hiddenLetter;
+wins.textContent = "Wins: " + count.winC;
+losses.textContent = "Losses: " + count.lossC;
+
+function newGame() {
+    var listOfWords = ["bootcamp", "coding", "language", "developer", "hypertext", "javascript", "cascade"];
+
+    var hiddenLetter = [];
+
+    var word = listOfWords[Math.floor(Math.random() * listOfWords.length)];
+
+    for (var i = 0; i < word.length; i++) {
+        hiddenLetter[i] = "_";
+        blank.textContent = hiddenLetter.join(" ");
+    }
+
+    let remaining = 10;
+    let guessedLetters = [];
+    guess.textContent = guessedLetters;
+    tries.textContent = "Tries Remaining: " + remaining;
+
+
+    document.onkeyup = function (event) {
+        var letter = event.key;
+
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] == letter) {
+                hiddenLetter[i] = letter;
+                blank.textContent = hiddenLetter.join(" ");
+            }
         }
+        if (word.indexOf(letter) < 0 && guessedLetters.indexOf(letter) < 0) {
+            remaining--;
+        }
+        if (remaining === 0) {
+            remaining = "You Lose!";
+            count.lossC++;
+        }
+        if (hiddenLetter.indexOf("_") === -1) {
+            remaining = "You Win!";
+            count.winC++;
+        }
+        if (guessedLetters.indexOf(event.key) === -1) {
+            guessedLetters.push(event.key);
+        }
+        guess.textContent = guessedLetters;
+        tries.textContent = "Tries Remaining: " + remaining;
+        wins.textContent = "Wins: " + count.winC;
+        losses.textContent = "Losses: " + count.lossC;
     }
-    if (word.indexOf(letter) < 0) {
-        remaining--;
-    }
-    if(remaining === 0) {
-        remaining = "You Lose!";
-    }
-    tries.textContent = remaining;
+
 }
+newGame();
 
 
-
-
+document.getElementById("newGame").onclick = function () { newGame() };
